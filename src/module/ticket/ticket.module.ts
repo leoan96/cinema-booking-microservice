c16001529d3c98f6b5ceb3b81c94b9fac449c368
Mun Kit <leoan96@hotmail.com>
2021-07-11 01:57:30 +0800

    implement basic ticket service

--- src/module/ticket/ticket.module.ts
+++ src/module/ticket/ticket.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Booking, BookingSchema } from '../booking/schema/booking.schema';
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Booking.name,
+        schema: BookingSchema,
+      },
+    ]),
+  ],
   controllers: [TicketController],
   providers: [TicketService],
   exports: [TicketService],
--- src/module/ticket/ticket.service.ts
+++ src/module/ticket/ticket.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Booking, BookingDocument } from '../booking/schema/booking.schema';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+  constructor(
+    @InjectModel(Booking.name)
+    private readonly bookingModel: Model<BookingDocument>,
+  ) {}
+
+  async getTicketById(ticketId: string): Promise<BookingDocument> {
+    return await this.bookingModel.findById(ticketId);
+  }
+}
