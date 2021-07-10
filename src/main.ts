56825f219ea66af4a31abe39e91077072e0d97d0
Mun Kit <leoan96@hotmail.com>
2021-07-10 18:04:42 +0800

    basic microservice setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as express from 'express';
+import * as httpContext from 'express-http-context';
+import { ConfigService } from '@nestjs/config';
+import { appConfiguration } from './app.configuration';
+import { setCorrelationId } from './shared/utils';
+import { CustomLogger } from './logger/custom-logger.logger';
+import { LoggingInterceptor } from './interceptor/logging.interceptor';
+import { Logger } from '@nestjs/common';
+import { AllExceptionFilter } from './filter/http-exception.filter';
+import { MongoExceptionFilter } from './filter/mongodb-exception.filter';
+import { RedisConnectService } from './module/redis/service/redis-connect.service';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useLogger(app.get(CustomLogger));
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const configService = app.get(ConfigService);
+  const session = app.get(RedisConnectService).getRedisSession();
+  const appConfig = appConfiguration(configService);
+
+  app.use(helmet());
+  app.enableCors(appConfig.cors);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalFilters(new MongoExceptionFilter());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.set('trust proxy', 1);
+  app.use(httpContext.middleware);
+  app.use(await session);
+  app.use(setCorrelationId);
+
+  const port = configService.get('app.port');
+  await app.listen(port);
+  logger.log(`Server listening on port ${port}...`);
 }
 bootstrap();
+
+process.on('uncaughtException', (error) => {
+  logger.error(
+    `UNCAUGHT EXCEPTION - keeping process alive:\n ${
+      error.stack
+    }\n ${JSON.stringify(error)}`,
+  );
+});
